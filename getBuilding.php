2092877f255956d15fb79647b7d576b9bbba0b9d
MisterTK <nathaphop.s@mail.kmutt.ac.th>
2019-11-23 20:27:48 +0700

    Edit variable name
    A little update

--- getBuilding.php
+++ getBuilding.php
@@ -5,7 +5,7 @@ $con = new databaseConnection();
 $con->connect();
 
 //Query data
-$sql = "SELECT * FROM `building` WHERE `building_Id` = '" . $buildingId . "'";
+$sql = "SELECT * FROM `building` WHERE `building_id` = '" . $buildingId . "'";
 $res = $con->query($sql);
 echo json_encode($res -> fetch_object());
   	
--- getFloor.php
+++ getFloor.php
@@ -5,7 +5,7 @@ $con = new databaseConnection();
 $con->connect();
 
 //Query data
-$sql = "SELECT * FROM `floor` WHERE `floor_Id` = '" . $floorgId . "'";
+$sql = "SELECT * FROM `floor` WHERE `floor_id` = '" . $floorId . "'";
 $res = $con->query($sql);
 echo json_encode($res -> fetch_object());
   	
