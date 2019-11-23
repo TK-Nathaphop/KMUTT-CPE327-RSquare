31beee0df51ca637674a1cf1de21ef874616f1d3
MisterTK <nathaphop.s@mail.kmutt.ac.th>
2019-11-23 21:39:26 +0700

    Debug and update progress of building management
    - Get all floor
    - Get building
    - Get floor
    - Get place

--- getFloor.php
+++ getFloor.php
@@ -1,11 +1,11 @@
 <?php
-  	$buildingId = $_GET['floorId'];
+  	$floorId = $_GET['floorId'];
   	require_once ('class/databaseConnection.php');
 $con = new databaseConnection();
 $con->connect();
 
 //Query data
-$sql = "SELECT * FROM `floor` WHERE `floor_id` = '" . $floorId . "'";
+$sql = "SELECT * FROM `floor` WHERE `flag` = true AND `floor_id` = '" . $floorId . "'";
 $res = $con->query($sql);
 echo json_encode($res -> fetch_object());
   	
