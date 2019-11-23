8f89b3e3e89323a61f000fa250a97bea07dd8da3
MisterTK <nathaphop.s@mail.kmutt.ac.th>
2019-11-24 01:04:51 +0700

    Update get all building

--- getAllBuilding.php
+++ getAllBuilding.php
@@ -8,6 +8,10 @@ $sql = "SELECT * FROM `building`";
 $res = $con->query($sql);
 $ret = array();
 while($item = $res -> fetch_object())
+{
+	if($item->picture == '')
+		$item->picture = 'nullbuilding.png';
 	$ret[] = $item;
+}
 echo json_encode($ret);	
 ?>
