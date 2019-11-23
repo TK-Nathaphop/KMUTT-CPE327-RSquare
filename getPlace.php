7ed1279e8eaccb919d2f577038596a64ac9952d2
MisterTK <nathaphop.s@mail.kmutt.ac.th>
2019-11-23 22:02:42 +0700

    Finish get place
    Finish the file get place

--- getPlace.php
+++ getPlace.php
@@ -9,8 +9,17 @@ $sql = "SELECT * FROM `place` WHERE `flag` = true AND `place_id` = '" . $placeId
 $res = $con->query($sql);
 $place = $res -> fetch_object();
 
+$sql = "SELECT * FROM `user` WHERE `user_id` = '" . $place->user_id . "'";
+$res = $con->query($sql);
+$user= $res -> fetch_object();
+$place->user_name = $user->name;
+$place->user_surname = $user->surname;
 //Query facility data
 $sql = "SELECT * FROM `facility` WHERE `flag` = true AND `place_id` = '" . $placeId . "'";
 $res = $con->query($sql);
-echo json_encode($ret)."<br>";
+$facility = array();
+while($item = $res -> fetch_object())
+	$facility[] = $item;
+$place->facility = $facility;
+echo json_encode($place)."<br>";
 ?>
