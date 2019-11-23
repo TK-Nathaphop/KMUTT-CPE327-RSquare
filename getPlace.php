8d07e1f4b4c490d5ca2011adbe9f2f463951137c
MisterTK <nathaphop.s@mail.kmutt.ac.th>
2019-11-24 03:51:21 +0700

    Update getPlace.php

--- getPlace.php
+++ getPlace.php
@@ -12,14 +12,7 @@ $place = $res -> fetch_object();
 $sql = "SELECT * FROM `user` WHERE `user_id` = '" . $place->user_id . "'";
 $res = $con->query($sql);
 $user= $res -> fetch_object();
-$place->user_name = $user->name;
-$place->user_surname = $user->surname;
-//Query facility data
-$sql = "SELECT * FROM `facility` WHERE `flag` = true AND `place_id` = '" . $placeId . "'";
-$res = $con->query($sql);
-$facility = array();
-while($item = $res -> fetch_object())
-	$facility[] = $item;
-$place->facility = $facility;
-echo json_encode($place)."<br>";
+$place->name = $user->name;
+$place->surname = $user->surname;
+echo json_encode($place);
 ?>
