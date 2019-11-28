903901ffdf3902abd0ebae9fe38a986cca2e9684
MisterTK <nathaphop.s@mail.kmutt.ac.th>
2019-11-28 14:33:30 +0700

    Debugging find activity and find organization

--- findOrganization.php
+++ findOrganization.php
@@ -1,10 +1,22 @@
 <?php
 
+//Get search from interface
+if(isset($_GET["search"]))
+    $search = $_GET["search"];
+else
+    $search = "";
+
+//Get search from interface
+if(isset($_GET["userId"]))
+    $userId = $_GET["userId"];
+else
+    die();
+
 require_once ('class/databaseConnection.php');
 $con = new databaseConnection();
 $con->connect();
 
-$res_act = $con->query("SELECT * FROM `organization`");
+$res_act = $con->query("SELECT * FROM `organization` WHERE `advisor` != '".$userId."' OR `advisor` IS NULL");
 $organizations = array();
 while($item = $res_act -> fetch_object())
 {
