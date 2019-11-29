3a21380956c1e4a26111dd05b753c132eeab0f3c
MisterTK <nathaphop.s@mail.kmutt.ac.th>
2019-11-29 11:03:43 +0700

    Display add building

---
+++ listBuildingRes.php
@@ -0,0 +1,51 @@
+<?php
+if(isset($_GET["userId"]))
+    $userId = $_GET["userId"];
+else
+    die();
+require_once ('class/databaseConnection.php');
+
+$con = new databaseConnection();
+$con->connect();
+
+//Query activity
+$sql = "SELECT * FROM `building`";
+$query_bd = $con->query($sql);
+$buildings = array();
+while($building = $query_bd -> fetch_object())
+{
+    $buildingId = $building->building_id;
+    $sql = "SELECT * FROM `floor` WHERE `building_id` = '".$buildingId."'" ;
+    $query_f = $con->query($sql);
+    $floors = array();
+    while($floor = $query_f -> fetch_object())
+    {
+        $floorId = $floor->floor_id;
+        $sql = "SELECT * FROM `place` WHERE `floor_id` = '".$floorId."'";
+        $query_p = $con->query($sql);
+        $places = array();
+        while($place = $query_p -> fetch_object())
+            $places[] = $place;
+        $floor->place = $places;
+        $floors[] = $floor;
+    }
+    $building->floor = $floors;
+    $buildings[] = $building;
+}
+
+foreach ($buildings as $building)
+{
+    foreach ($building->floor as $floor)
+    {
+        foreach ($floor->place as $place)
+        {
+            if($place->user_id == $userId)
+                $place->res = true;
+            else
+                $place->res = false;
+        }
+    }
+}
+
+echo json_encode($buildings);   
+?>
