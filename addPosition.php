e52403cdd0d45fe1e8f6df545fe92492b32b812f
MisterTK <nathaphop.s@mail.kmutt.ac.th>
2019-12-01 02:57:34 +0700

    Edit position in user management

---
+++ addPosition.php
@@ -0,0 +1,13 @@
+<?php
+
+$rawpost = file_get_contents('php://input');
+$json_txt = str_replace("\\", "", $rawpost);
+$json = json_decode($json_txt);
+require_once ('class/databaseConnection.php');
+$con = new databaseConnection();
+$con->connect();
+$position = $json->position;
+$userId = $json->user_id;
+$sql = "UPDATE `user` SET `position`= '".$position."' WHERE `user_id` = '". $userId ."'";
+$con->query($sql);
+?>
