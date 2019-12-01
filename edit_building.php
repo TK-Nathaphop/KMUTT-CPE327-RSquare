6a88c4cd886c2965cc97b838bb0774fece5f662a
MisterTK <nathaphop.s@mail.kmutt.ac.th>
2019-12-01 08:26:29 +0700

    edit building

--- edit_building.php
+++ edit_building.php
@@ -84,6 +84,10 @@ foreach($json[0]->floor as $floor)
     	    	$speaker = 'true';
     	    else
     	    	$speaker = 'false';
+    	    if($place->placeCapacity == null)
+        		$	capacity = 0;
+        	else
+        		$capacity = $place->placeCapacity;
 	
 			if($place->id == null)
 			{
@@ -101,26 +105,26 @@ foreach($json[0]->floor as $floor)
 				$condition_p[] = "`place_id` != '".$place->id."'";
 			}
 		}
+		if(count($condition_p) != 0)
+		{
+			$command = $condition_p[0];
+			for($i=1; $i<count($condition_p); $i++)
+				$command = $command.' AND '.$condition_p[$i];
+			$sql = "UPDATE `place` SET `flag` = 0 WHERE ".$command." AND `floor_id` = '".$floor_id."'";
+			echo $sql;
+			$con->query($sql);
+		}
 		$condition_f[] = "`floor_id` != '".$floor->id."'";
 	}
-	if(count($condition_p) != 0)
-	{
-		$command = $condition_p[0];
-		for($i=1; $i<count($condition_p); $i++)
-			$command = $command.' AND '.$condition_p[$i];
-		$sql = "UPDATE `place` SET `flag` = 0 WHERE ".$command;
-		echo $sql;
-		$con->query($sql);
-	}
-	if(count($condition_f) != 0)
-	{
-		$command = $condition_f[0];
-		for($i=1; $i<count($condition_f); $i++)
-			$command = $command.' AND '.$condition_f[$i];
-		$sql = "UPDATE `floor` SET `flag` = 0 WHERE ".$command;
-		echo $sql;
-		$con->query($sql);
-	}
+}
+if(count($condition_f) != 0)
+{
+	$command = $condition_f[0];
+	for($i=1; $i<count($condition_f); $i++)
+		$command = $command.' AND '.$condition_f[$i];
+	$sql = "UPDATE `floor` SET `flag` = 0 WHERE ".$command." AND `building_id` = '".$building_id."'";
+	echo $sql;
+	$con->query($sql);
 }
 $con->disconnect();
 ?>
